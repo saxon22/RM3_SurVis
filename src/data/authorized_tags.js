57011309df7e71fda4e13988bffc768d3ff97cc1
Saxon Campbell <saxoncampbell@Saxons-MacBook-Air.local>
2024-05-11 21:13:50 +0100

    Commit

---
+++ src/data/authorized_tags.js
@@ -0,0 +1,10 @@
+define({
+    authorizedTags: {
+        "type:survey": {
+            "description": "paper describing a survey"
+        },
+        "type:article": {
+            "description": "paper describing an article"
+        }
+    }
+});
